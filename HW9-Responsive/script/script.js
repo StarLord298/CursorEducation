12296535706e7b50cba6bc07dd2f3f68f7698eac
StarLord298 <4reient4@gmail.com>
2020-10-04 20:30:15 +0300

    painted dots

---
+++ HW9-Responsive/script/script.js
@@ -0,0 +1,11 @@
+const dots = document.querySelectorAll(".dot");
+
+for (let i = 0; i <= dots.length; i++) {
+  dots[0].style.background = "#828282";
+  dots[1].style.background = "#979595";
+  dots[2].style.background = "#B2B0B0";
+  dots[3].style.background = "#CDCBCB";
+  dots[4].style.background = "#E2E1E1";
+  dots[5].style.background = "#EEEEEE";
+  dots[6].style.background = "#F7F7F7;";
+}
