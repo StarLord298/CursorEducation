0910527be8bc4a7c2b19791ec33f60008e6dc119
StarLord298 <4reient4@gmail.com>
2020-10-10 18:12:28 +0300

    changed toggle classes

--- HW11-Animation/script.js
+++ HW11-Animation/script.js
@@ -1,16 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
-const first = document.querySelector("#first-right");
-const second = document.querySelector("#second-right");
-const thirt = document.querySelector("#thirt-right");
 
+const f = document.querySelectorAll(".right");
 
 
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger_active");
-    first.classList.toggle("firstpart-right");
-    second.classList.toggle("secondpart-right");
-    thirt.classList.toggle("thirtpart-right");
+    f.forEach(element  => {
+        f[0].classList.toggle("firstpart-right");
+        f[1].classList.toggle("secondpart-right");
+        f[2].classList.toggle("thirtpart-right");
+    });
 });
 
 
